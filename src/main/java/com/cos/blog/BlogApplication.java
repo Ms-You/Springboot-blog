c437a25e27c1cee991da6e41917cb91e6b79bf31
Myeongsu Yu <msu122005@hs.ac.kr>
2022-03-17 17:28:45 +0900

    create domain layer
    create User, Board, Reply domain model

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -2,8 +2,10 @@ package com.cos.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// Security 웹 보안 자동 설정 끔
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BlogApplication {
 
 	public static void main(String[] args) {
