f74eb713093cb447cf51e9004bd999026c76eba6
Myeongsu Yu <msu122005@hs.ac.kr>
2022-03-18 01:15:39 +0900

    create a class GlobalExceptionHandler
    create a class that only excepts IllegalArgumentException

---
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.cos.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice   // 모든 Exception 을 이 클래스에서 처리
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public String handleArgumentException(IllegalArgumentException e){
+        return "<h1>"+e.getMessage()+"</h1>";
+    }
+}
